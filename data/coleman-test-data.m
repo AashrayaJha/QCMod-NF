c3fa5b6e98fb587f3a303969601711b2869f03c8
Jennifer Balakrishnan <jbala@bu.edu>
2024-02-11 14:09:32 -0500

    edited print statements

---
+++ data/coleman-test-data.m
@@ -0,0 +1,7 @@
+coleman_data_format := recformat<Q, p, N, g, W0, Winf, r, Delta, s, G0, Ginf, e0, einf, delta, basis, quo_map, integrals, F, f0list, finflist, fendlist, Nmax, red_list_fin, red_list_inf, minpolys, cpm, subspace, ordinary, frobmatb0r>;
+
+coleman_data_list := [* *];
+
+_<x> := PolynomialRing(RationalField());
+_<z> := LaurentSeriesRing(PolynomialRing(RationalField()));
+
