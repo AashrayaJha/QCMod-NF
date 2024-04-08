7e3bb44223da1d200dd88cfb4d16236a38a9909b
Jennifer Balakrishnan <jbala@bu.edu>
2024-04-08 14:13:41 -0400

    Update hensel.m

--- hensel.m
+++ hensel.m
@@ -112,7 +112,6 @@ function two_variable_padic_system_solver(G, H, p, prec1, prec2)
 //    by combining naive lifting of roots with the multivariable
 //    Hensel's lemma. See Appendix A, Algorithm 1 (4) of [BBBM19].
 
-/* work in progress
 
 K := pAdicField(p,prec2);
 sols := [];
@@ -121,7 +120,7 @@ Zxy<x,y> := PolynomialRing(Integers(), 2);
 gprec := Zxy!G;
 hprec := Zxy!H;
 
-#Find roots modulo p^prec1 by naive lifting
+//Find roots modulo p^prec1 by naive lifting
 for i in [1..prec1] do
     modulus_one_less := p^(i-1);
     tempsols := [];
@@ -170,14 +169,92 @@ for i in [1..prec1] do
         fct_list := temp_fct_list;
         new_list := temp_new_list;
 
-#Reduce the roots modulo prec1-3 to avoid spurious sols
-sols := [(K!x + O(K!p^(prec1-3)), K!y + O(K!p^(prec1-3))) : (x,y) in new_list]
-sols ;= sorted(set(sols));
+// Reduce the roots modulo prec1-3 to avoid spurious sols
+sols := [(K!x + O(K!p^(prec1-3)), K!y + O(K!p^(prec1-3))) : (x,y) in new_list];
+sols := sorted(set(sols));
 
-#Now apply multivariable Hensel on the roots that are
-#simple modulo prec1-3
+// Now apply multivariable Hensel on the roots that are
+// simple modulo prec1-3
 flist := [G,H];
 precvec := [];
-k := 0;
+k := 1;
+for F in flist do
+    R1 := Parent(flist[k]);
+    F1 := BaseRing(R1);
+    if IsExactpAdic(F1) then
+        precision1 := prec2;
+    else
+        precision1 := Precision(F1)
+        if prec2 gt  precision1:
+            print "Cannot get %o digits of precision due to the precision of inputs of f1; raise precision of inputs", prec;
+        elif prec2 lt precision1:
+            precision1 := prec2;
+        end if;
+    end if;
+    Append(~precvec, precision1);
+    k := k+1;
+end for;
 
-/*
+precision := Min(precvec);
+
+
+R := PolynomialRing(pAdicField(p,precision), #flist);
+flistnew := [];
+for F in flist do
+    F := R!F;
+    Append(~flistnew, F);
+end for;
+Jlist := [];
+for F in flistnew do
+    for i in [1..#flistnew] do
+        Append(~Jlist, Derivative(F,i));
+    end for;
+end for;
+J := Matrix(#flistnew, #flistnew, Jlist);
+M := Determinant(J);
+roots := [];
+roots_info := [];
+roots2 := [];
+for i in [1..#sols] do
+    valuesval := [Valuation(Evaluate(F, sols[i])): F in flistnew];
+    min_valuesval := Min(valuesval);
+    ord_det_J := Valuation(Evaluate(M, sols[i]));
+    if min_valuesval gt 2*ord_det_J then
+        Append(~roots, sols[i]);
+        Append(~roots_info, [min_valuesval - 2*ord_det_J,ord_det_J]);
+    else
+        Append(~roots2, sols[i]);
+    end if;
+end for;
+actual_roots := roots2;
+for r in roots do
+    ind_roots := Index(roots, r);
+    rt_info := roots_info[ind_roots];
+    if not IsFinite(rt_info[1]) then
+        Append(~actual_roots,(K!Integers()!(r[1]),K!Integers()!(r[2])));
+    else
+        ind_roots := Index(roots,r);
+        rt_info := roots_info[ind_roots];
+        variables := [];        
+        r := [Integers()!r[1], Integers()!r[1]];
+        i_l := Matrix(#flist, 1, r);
+        Jeval := Matrix(#flistnew,#flistnew ,[Evaluate(f,r) : f in Jlist]);
+        B := Transpose(Jeval)*Jeval;
+        const2:=Ceiling(Log( ((prec2-rt_info[2])/rt_info[1]))/Log(2.)) + 1 ;
+        k := 0;
+        while k  lt const2 and Determinant(B) ne 0 do
+            A := Matrix(#flistnew, 1, [-Evaluate(f, r): f in flistnew]);
+            i_l := i_l + B^(-1)*Transpose(Jeval)*A;
+            for i in [1..#flist] do
+                Append(~variables, i_l[i,1]);
+            end for;
+            r := variables;
+            variables := [];
+            k := k+1;
+            Jeval := Matrix(#flistnew, #flistnew, [Evaluate(f,r): f in Jlist]);
+            B := Transpose(Jeval)*Jeval;
+        end while;
+        Append(~actual_roots,(K!(i_l[1][1]), K!(i_l[2][1])));
+    end if;
+end for;
+return actual_roots, #roots2;
