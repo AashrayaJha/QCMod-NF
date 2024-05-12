0d91f1e683f74b9cde57e89ca5045b72798d5227
Jennifer Balakrishnan <jbala@bu.edu>
2024-05-12 10:07:00 -0400

    Update NF-coleman-collect-patch2.m

--- data/NF-coleman-collect-patch2.m
+++ data/NF-coleman-collect-patch2.m
@@ -10,7 +10,6 @@ import "misc.m": are_congruent, equivariant_splitting, eval_mat_R, eval_Q, FindQ
 import "applications.m": Q_points, Qp_points, roots_with_prec, separate;
 import "heights.m": E1_tensor_E2, expand_algebraic_function, frob_equiv_iso, height;
 
-t1: = Cputime();
 K<u> := CyclotomicField(3);
 OK := Integers(K);
 Kx<x> := PolynomialRing(K);
@@ -18,8 +17,9 @@ Kxy<y> := PolynomialRing(Kx);
 Q := y^4 + ((-14*u + 9)*x - u + 1)*y^3 - 3*x^2*y^2 + ((-2330*u - 2662)*x^3 + (-663*u - 726)*x^2 + (-63*u - 66)*x - 2*u - 2)*y + (-978*u + 12)*x^4 + (-298*u + 1)*x^3 - 30*u*x^2 - u*x;
 p := 13;
 v := Factorization(p*OK)[2][1];
-N := 200 ; //Going to try 40
+N := 40 ;
 
+t1 := Cputime();
 "Constructing symplectic basis of H1...";
 h1basis, g, r, W0 := H1Basis(Q, v);
 prec := 2*g;
@@ -30,13 +30,18 @@ sympl_basis := [h1basis[i] : i in [1..g]] cat new_complementary_basis;
 basis0 := [[sympl_basis[i,j] : j in [1..Degree(Q)]] : i in [1..g]]; // basis of regular differentials
 basis1 := [[sympl_basis[i,j] : j in [1..Degree(Q)]] : i in [g+1..2*g]];  // basis of complementary subspace
 "Symplectic basis constructed.";
+t2 := Cputime();
+t2-t1;
 
+t1 := Cputime();
 "Constructing Coleman data...";
 time data := ColemanData(Q, v, N : useU:=false,  basis0:=basis0, basis1:=basis1);
 "Coleman data constructed.";
+t2 := Cputime();
+t2 - t1;
 
 "Recording Coleman data...";
-output_file := "data/NF-example-coleman-data-13_patch2_200.m";
+output_file := "data/NF-example-coleman-data-13_patch2_40.m";
 fprintf output_file, "K<u> := CyclotomicField(3);\n";
 fprintf output_file, "_<x> := PolynomialRing(K);\n";
 fprintf output_file, "_<z> := LaurentSeriesRing(PolynomialRing(K));\n\n";
@@ -47,6 +52,5 @@ out := ReplaceString(out, "EquationOrder(Polynomial(\\[1, 1, 1]))", "Integers(Cy
 
 Write(output_file,out);
 "Coleman data recorded.";
-t2:=Cputime();
-t2 - t1;
+
 
