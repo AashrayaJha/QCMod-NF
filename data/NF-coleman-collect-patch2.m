7fdf6a3cd05dcaed82383159e3d31bde940182c0
Jennifer Balakrishnan <jbala@bu.edu>
2024-05-13 21:39:01 -0400

    Update NF-coleman-collect-patch2.m

--- data/NF-coleman-collect-patch2.m
+++ data/NF-coleman-collect-patch2.m
@@ -9,6 +9,7 @@ import "singleintegrals.m": evalf0, is_bad, local_coord, set_point, tadicprec, t
 import "misc.m": are_congruent, equivariant_splitting, eval_mat_R, eval_Q, FindQpointQp, fun_field, alg_approx_Qp, minprec, minval, minvalp;
 import "applications.m": Q_points, Qp_points, roots_with_prec, separate;
 import "heights.m": E1_tensor_E2, expand_algebraic_function, frob_equiv_iso, height;
+//import "string-replace.m": ReplaceString;
 
 K<u> := CyclotomicField(3);
 OK := Integers(K);
@@ -17,7 +18,7 @@ Kxy<y> := PolynomialRing(Kx);
 Q := y^4 + ((-14*u + 9)*x - u + 1)*y^3 - 3*x^2*y^2 + ((-2330*u - 2662)*x^3 + (-663*u - 726)*x^2 + (-63*u - 66)*x - 2*u - 2)*y + (-978*u + 12)*x^4 + (-298*u + 1)*x^3 - 30*u*x^2 - u*x;
 p := 13;
 v := Factorization(p*OK)[2][1];
-N := 40 ;
+N := 200 ;
 
 t1 := Cputime();
 "Constructing symplectic basis of H1...";
@@ -48,8 +49,8 @@ fprintf output_file, "_<x> := PolynomialRing(K);\n";
 fprintf output_file, "_<z> := LaurentSeriesRing(PolynomialRing(K));\n\n";
 
 out := Sprintf("data := %m;\n\n", data);
-out := ReplaceString(out, "\n ! ", "\n "); // hack to handle bug in Magma string formatting
-out := ReplaceString(out, "EquationOrder(Polynomial(\\[1, 1, 1]))", "Integers(CyclotomicField(3))");
+//out := ReplaceString(out, "\n ! ", "\n "); // hack to handle bug in Magma string formatting
+//out := ReplaceString(out, "EquationOrder(Polynomial(\\[1, 1, 1]))", "Integers(CyclotomicField(3))");
 
 Write(output_file,out);
 "Coleman data recorded.";
