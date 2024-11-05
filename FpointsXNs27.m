77c8a399fe2acfa0284eccc8f3c1590f4731930e
AashrayaJha <90110260+AashrayaJha@users.noreply.github.com>
2024-11-05 11:54:34 -0500

    Revert "Fixed points"
    This reverts commit e1798ace247041c3ef0e4d8f09cb708ee1c46e7e.

--- FpointsXNs27.m
+++ FpointsXNs27.m
@@ -222,11 +222,12 @@ XH := Curve(ProjectiveSpace(K,2),pol);
 
 // Staring at the Fourier expansions of modular forms that David Zywina's code produces 
 // and matching those with the modular forms for the subgroup H whose determinant isn't
-// surjective yields the following birational map. (Given to us by Jeremy Rouse)
+// surjective yields the following map.
 
-phi1 := map<Xns27K -> XH | [(1/3)*((-2-zeta)*Xns27K.11 + (1-zeta)*Xns27K.12), 
+phi := map<Xns27K -> XH | [(1/3)*((-2-zeta)*Xns27K.11 + (1-zeta)*Xns27K.12), 
 (-1/3)*((1+2*zeta)*Xns27K.9 + (-1+zeta)*Xns27K.10),
 (-1/3)*((1+2*zeta)*Xns27K.7 + (1-zeta)*Xns27K.8)]>;
+u:=zeta;
 
 //Changing the coordinates back to RSZB format, which we had initially changed to use Tuitman's algorithm.
 points:= [XH![P[2],P[1],P[3]] : P in [
@@ -247,91 +248,76 @@ points:= [XH![P[2],P[1],P[3]] : P in [
 
 CM_points:=[points[i]: i in [1..12]];
 non_CMpoint:=points[13];
-
-//The map phi1 has base locus a reduced 0 dimensional scheme of degree 10, and one of the points P
-//is defined over Q(zeta_3). The conjugate of P is in the preimage of points[9] under phi1.
-
+non_CMns27:=RationalPoints(non_CMpoint@@phi);
 //CMj := [0,1728,-3375,8000,-32768,54000,287496,-884736,-12288000,16581375,-884736000,-147197952000,-262537412640768000];
+Rationalpts:=[**];
+Kpoints:=[**];
+KpointsCM:=[**];
+
+x:=0;
+for pt in points do
+    pt_preimage_list := RationalPoints(pt@@phi);
+#pt_preimage_list;
+    for ptinv in pt_preimage_list do
+        Append(~Kpoints,ptinv);
+        try
+            rtlptinv:=Xns27![Rationals()!ptinv[i]: i in [1..12]];
+            Append(~Rationalpts,rtlptinv);
+            catch e
+                x:=x+1;
+        end try;            
+    end for;    
+end for;   
+
+for pt in CM_points do
+    pt_preimage_list := RationalPoints(pt@@phi);
+    for ptinv in pt_preimage_list do
+        Append(~KpointsCM,ptinv);      
+    end for;    
+end for;   
+
+Kpointsseq := SetToSequence(SequenceToSet([P : P in Kpoints]));
+KpointsCMseq := SetToSequence(SequenceToSet([P : P in KpointsCM]));
 
 P1 := ProjectiveSpace(K,1);
-// phi2 is the map XH to Xns+(9).
+
 phi2 := map<XH -> P1 | [[(zeta-1)*XH.1+(-2*zeta-1)*XH.2+(zeta-1)*XH.3,(-zeta-2)*XH.1+(-zeta-2)*XH.2+(-zeta+1)*XH.3],[1/3*(-zeta + 1)*XH.1^3 + 1/3*(4*zeta - 1)*XH.1^2*XH.2 + 1/3*(-4*zeta + 1)*XH.1*XH.2^2
     + (zeta + 1)*XH.2^3 + 1/3*(2*zeta + 4)*XH.1^2*XH.3 + zeta*XH.1*XH.2*XH.3 + 
     1/3*(-8*zeta - 4)*XH.2^2*XH.3 + 1/3*(zeta - 1)*XH.1*XH.3^2 + 1/3*(5*zeta + 
     1)*XH.2*XH.3^2 + 1/3*(-2*zeta - 1)*XH.3^3,XH.2^3 + (zeta - 1)*XH.2^2*XH.3 + (-2*zeta - 1)*XH.2*XH.3^2]]>;
 
-// The follwoing will be the coordinates of a morphism from Xns+(27) to Xns+(9)
-des1 := [(zeta+1)*Xns27K.7 - zeta*Xns27K.8 - Xns27K.9 + (-zeta-1)*Xns27K.10 + Xns27K.11 + zeta*Xns27K.12,
-(-zeta-1)*Xns27K.7 + zeta*Xns27K.8 + zeta*Xns27K.9 - Xns27K.10 + (zeta+1)*Xns27K.11 - Xns27K.12];
-des2 := [(zeta^2+1)*Xns27K.7 - zeta^2*Xns27K.8 - Xns27K.9 + (-zeta^2-1)*Xns27K.10 + Xns27K.11 + zeta^2*Xns27K.12,
-(-zeta^2-1)*Xns27K.7 + zeta^2*Xns27K.8 + zeta^2*Xns27K.9 - Xns27K.10 + (zeta^2+1)*Xns27K.11 - Xns27K.12];
-des3 := [
-        1/9*Xns27K.7^3 + 1/3*(-zeta - 1)*Xns27K.7^2*Xns27K.8 + 1/3*zeta*Xns27K.7*Xns27K.8^2 + 1/9*Xns27K.8^3 +
-            1/9*(-zeta - 3)*Xns27K.7^2*Xns27K.9 + 1/9*(6*zeta + 4)*Xns27K.7*Xns27K.8*Xns27K.9 + 
-            1/9*(-2*zeta + 1)*Xns27K.8^2*Xns27K.9 + 4/9*Xns27K.7*Xns27K.9^2 + 1/9*(-4*zeta - 
-            4)*Xns27K.8*Xns27K.9^2 + 1/9*(zeta - 1)*Xns27K.9^3 + 1/9*(-3*zeta - 2)*Xns27K.7^2*Xns27K.10 +
-            1/9*(4*zeta - 2)*Xns27K.7*Xns27K.8*Xns27K.10 + 1/9*(zeta + 3)*Xns27K.8^2*Xns27K.10 + 
-            1/9*(8*zeta + 8)*Xns27K.7*Xns27K.9*Xns27K.10 - 8/9*zeta*Xns27K.8*Xns27K.9*Xns27K.10 + 1/3*(-zeta -
-            2)*Xns27K.9^2*Xns27K.10 + 4/9*zeta*Xns27K.7*Xns27K.10^2 + 4/9*Xns27K.8*Xns27K.10^2 + 1/3*(-2*zeta 
-            - 1)*Xns27K.9*Xns27K.10^2 + 1/9*(-zeta + 1)*Xns27K.10^3 - 1/9*Xns27K.7^2*Xns27K.11 + 
-            1/9*(2*zeta + 2)*Xns27K.7*Xns27K.8*Xns27K.11 - 1/9*zeta*Xns27K.8^2*Xns27K.11 + 1/9*(zeta - 
-            1)*Xns27K.7*Xns27K.9*Xns27K.11 + 1/9*(zeta + 2)*Xns27K.8*Xns27K.9*Xns27K.11 + 1/9*(-zeta + 
-            2)*Xns27K.9^2*Xns27K.11 + 1/9*(-zeta - 2)*Xns27K.7*Xns27K.10*Xns27K.11 + 1/9*(2*zeta + 
-            1)*Xns27K.8*Xns27K.10*Xns27K.11 + 1/9*(4*zeta + 6)*Xns27K.9*Xns27K.10*Xns27K.11 + 1/9*(3*zeta + 
-            1)*Xns27K.10^2*Xns27K.11 + 2/9*Xns27K.7*Xns27K.11^2 + 1/9*(-2*zeta - 2)*Xns27K.8*Xns27K.11^2 + 
-            1/9*(3*zeta + 1)*Xns27K.9*Xns27K.11^2 + 1/9*(zeta - 2)*Xns27K.10*Xns27K.11^2 + 
-            1/9*(-zeta - 1)*Xns27K.11^3 - 1/9*zeta*Xns27K.7^2*Xns27K.12 - 2/9*Xns27K.7*Xns27K.8*Xns27K.12 + 
-            1/9*(zeta + 1)*Xns27K.8^2*Xns27K.12 + 1/9*(-2*zeta - 1)*Xns27K.7*Xns27K.9*Xns27K.12 + 
-            1/9*(zeta - 1)*Xns27K.8*Xns27K.9*Xns27K.12 + 1/9*(3*zeta + 1)*Xns27K.9^2*Xns27K.12 + 
-            1/9*(-zeta + 1)*Xns27K.7*Xns27K.10*Xns27K.12 + 1/9*(-zeta - 2)*Xns27K.8*Xns27K.10*Xns27K.12 + 
-            1/9*(2*zeta - 4)*Xns27K.9*Xns27K.10*Xns27K.12 + 1/9*(-2*zeta - 3)*Xns27K.10^2*Xns27K.12 + 
-            4/9*zeta*Xns27K.7*Xns27K.11*Xns27K.12 + 4/9*Xns27K.8*Xns27K.11*Xns27K.12 + 1/9*(-4*zeta - 
-            6)*Xns27K.9*Xns27K.11*Xns27K.12 + 1/9*(-6*zeta - 2)*Xns27K.10*Xns27K.11*Xns27K.12 + 
-            1/3*Xns27K.11^2*Xns27K.12 + 1/9*(-2*zeta - 2)*Xns27K.7*Xns27K.12^2 + 2/9*zeta*Xns27K.8*Xns27K.12^2
-            + 1/9*(-zeta + 2)*Xns27K.9*Xns27K.12^2 + 1/9*(2*zeta + 3)*Xns27K.10*Xns27K.12^2 + 
-            1/3*zeta*Xns27K.11*Xns27K.12^2 + 1/9*(-zeta - 1)*Xns27K.12^3,
-        1/3*Xns27K.7^2*Xns27K.9 + 1/3*(-2*zeta - 2)*Xns27K.7*Xns27K.8*Xns27K.9 + 1/3*zeta*Xns27K.8^2*Xns27K.9 + 
-            1/3*(-zeta - 1)*Xns27K.7*Xns27K.9^2 + 1/3*zeta*Xns27K.8*Xns27K.9^2 + 1/9*(2*zeta + 
-            1)*Xns27K.9^3 + 1/3*(zeta + 1)*Xns27K.7^2*Xns27K.10 - 2/3*zeta*Xns27K.7*Xns27K.8*Xns27K.10 - 
-            1/3*Xns27K.8^2*Xns27K.10 - 2/3*zeta*Xns27K.7*Xns27K.9*Xns27K.10 - 2/3*Xns27K.8*Xns27K.9*Xns27K.10 + 
-            1/3*(zeta - 1)*Xns27K.9^2*Xns27K.10 + 1/3*Xns27K.7*Xns27K.10^2 + 1/3*(-zeta - 
-            1)*Xns27K.8*Xns27K.10^2 + 1/3*(-zeta - 2)*Xns27K.9*Xns27K.10^2 + 1/9*(-2*zeta - 
-            1)*Xns27K.10^3
-    ];
-// Morphism from X_{ns}^+(27) -> X_{ns}^+(9), one can check the Degree(BaseScheme(phi)) has degree 0.
+// Map from X_ns^+(9) to the j-line
+phi3 := map<P1 -> P1 | [(-15*P1.1^9 - 81*P1.1^8*P1.2 - 27*P1.1^7*P1.2^2 + 117*P1.1^6*P1.2^3 
+	- 81*P1.1^5*P1.2^4 + 189*P1.1^4*P1.2^5 - 288*P1.1^3*P1.2^6 + 216*P1.1*P1.2^8 - 96*P1.2^9)^3,
+(P1.1^9 - 9*P1.1^7*P1.2^2 + 3*P1.1^6*P1.2^3 + 27*P1.1^5*P1.2^4 - 18*P1.1^4*P1.2^5 - 
+	24*P1.1^3*P1.2^6 + 27*P1.1^2*P1.2^7 - 9*P1.1*P1.2^8 + P1.2^9)^3]>;
 
-assert Degree(BaseScheme(phi)) eq 0;
-phi := map<Xns27K -> P1 | [des1,des2,des3]>;
-assert Degree(BaseScheme(phi)) eq 0;
 
-//Will now pullback points from Xns+(9) to Xns+(27) which are in the image of phi2.
-K_pts_27:=[**];
-Q_pts_27:=[**];
-// This map is a morphism since Degree(BaseScheme(phi)) is 0.
-CM_pts_27:=[**];
-x:=0; //Counting Q(zeta_3) points
 
-K_pts_9:=SetToSequence(Set([phi2(P): P in points]));
-CM_pts_9:=SetToSequence(Set([phi2(P): P in CM_points]));
 
-for P in K_pts_9 do
-    printf "Doing point %o now.\n", P;
-    pt_preimage_list := RationalPoints(P@@phi);
-    printf "The points in the preimage are \n %o. \n", pt_preimage_list;
-    for ptinv in pt_preimage_list do
-        Append(~K_pts_27,ptinv);
-        if P in CM_pts_9 then
-            Append(~CM_pts_27,ptinv);
-        end if;    
-        try
-            rtlptinv:=Xns27![Rationals()!ptinv[i]: i in [1..12]];
-            Append(~Q_pts_27,rtlptinv);
-        catch e
-                x:=x+1;
-        end try;            
-    end for;    
-end for; 
+printf "The number of Q(zeta_3) points of X_ns^+(27) is %o, and of these %o are Q-points.\n", #Kpointsseq,#Rationalpts;
+printf "X_{ns}^+(27) has %o Q(zeta_3)-points which are CM. \n", #KpointsCMseq;
+printf "There are %o non-CM point (s), which are \n %o.\n" , #non_CMns27, non_CMns27;
+// jinvlist:=[]; 
+// x:=0 ; //CMcount
+ 
+// pt:=points[1];
+// jinvar := phi3(phi2(pt))[1];
+// chk, disc := HasComplexMultiplication(EllipticCurveWithjInvariant(jinvar));
+//      if chk  then
+//         x:=x+1;
+//         Append(~jinvlist, jinvar);
+//      else
+//         Append(~jinvlist, jinvar);
+//      end if;    
+
+// RF:=recformat<pt:Pt, jinv: FldCycElt>;
+// jinvlist:=rec<RF|>;
     
-printf "The number of Q(zeta_3) points of X_ns^+(27) is %o, and of these %o are Q-points.\n", #K_pts_27,#Q_pts_27;
-printf "The number of CM Q(zeta_3) points is %o. \n", #CM_pts_27;
 
+// for pt in points do
+//     pt_preimage := RationalPoints(pt@@phi);
+// //   end for;
+// //   printf "The points in XH(K) above j = %o are %o.\n",curj,X3list;
+// //   printf "The points on Xns+(27)(K) above j = %o are %o.\n",curj,X4list;
+// end for;  
